c0b6ecdd7ea670e1d4f5fe3ff9cc1c984e5fc75e
Gaurav Khandelwal <gauravkhandelwal327@gmail.com>
2025-11-12 15:51:54 +0530

    redirect root url to dashboard & fix static files

--- farmsetu_weather/urls.py
+++ farmsetu_weather/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from weatherapp import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/dashboard/', permanent=True)),
     path('admin/', admin.site.urls),
     path('api/', include('weatherapp.urls')),
     path('dashboard/', views.dashboard, name='dashboard'),
